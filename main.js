31b7d028f3bd7354f45faa8640efd64a6e37e6cd
Mike <mstirmii@gmail.com>
2021-03-14 15:50:55 -0400

    first commit for opasite

---
+++ main.js
@@ -0,0 +1,18 @@
+$(function() {
+
+  var $window = $(window);
+
+  $('main[data-type="background"]').each(function() {
+
+    var $bgobj = $(this);
+
+    $(window).scroll(function() {
+
+      var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+      var coords = '50% '+ yPos + 'px';
+
+      $bgobj.css({ backgroundPosition: coords });
+    });
+  });
+});
